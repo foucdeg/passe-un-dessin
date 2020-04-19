b0d26b4e77bfac4b224a4e2277a5c16485ebce69
Foucauld Degeorges <foucdeg@gmail.com>
2020-04-19 19:53:35 +0200

    Add rule warning about internet connection

--- frontend/src/assets/rule-backgrounds/index.ts
+++ frontend/src/assets/rule-backgrounds/index.ts
@@ -2,5 +2,12 @@ import rule1Background from './rule-1.png';
 import rule2Background from './rule-2.png';
 import rule3Background from './rule-3.png';
 import rule4Background from './rule-4.png';
+import rule5Background from './rule-5.png';
 
-export default [rule1Background, rule2Background, rule3Background, rule4Background];
+export default [
+  rule1Background,
+  rule2Background,
+  rule3Background,
+  rule4Background,
+  rule5Background,
+];
