a7e62067d4675c63a784a7055b519c2ac51e44c9
RobotScribe <quentinso@theodo.fr>
2020-04-24 20:14:48 +0200

    :ok_hand: :recycle: Put TopRightButtons in atoms

---
+++ frontend/src/atoms/TopRightButtons.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const TopRightButtons = styled.div`
+  position: absolute;
+  right: 0;
+  top: -40px;
+`;
+
+TopRightButtons.displayName = 'TopRightButtons';
+
+export default TopRightButtons;
