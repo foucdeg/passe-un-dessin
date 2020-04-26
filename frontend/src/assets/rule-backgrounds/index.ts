e5d3e6a148bb589203e338bc878fcdc789aa5559
RobotScribe <quentinso@theodo.fr>
2020-04-26 22:24:15 +0200

    :bento: create new rule to avoid React key error

--- frontend/src/assets/rule-backgrounds/index.ts
+++ frontend/src/assets/rule-backgrounds/index.ts
@@ -3,7 +3,7 @@ import rule2Background from './rule-2.png';
 import rule3Background from './rule-3.png';
 import rule4Background from './rule-4.png';
 import rule5Background from './rule-5.png';
-import rule6Background from './rule-2.png';
+import rule6Background from './rule-6.png';
 
 export default [
   rule1Background,
