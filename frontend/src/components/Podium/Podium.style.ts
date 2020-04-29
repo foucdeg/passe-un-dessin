6bafa068d6b5c147f426987d99011dba067a82bb
Foucauld Degeorges <foucdeg@gmail.com>
2020-04-29 14:22:37 +0200

    reduce hardcoded pixels

--- frontend/src/components/Podium/Podium.style.ts
+++ frontend/src/components/Podium/Podium.style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: relative;
+  display: flex;
 `;
 Container.displayName = 'Container';
--- frontend/src/components/Podium/Podium.tsx
+++ frontend/src/components/Podium/Podium.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from './Podium.style';
 import { PadStep } from 'redux/Game/types';
-import podium from 'assets/podium.svg';
 
 import PodiumStep from 'components/PodiumStep';
 
@@ -12,16 +11,9 @@ interface Props {
 const Podium: React.FC<Props> = ({ winners }) => {
   return (
     <Container>
-      <img src={podium} alt="Podium" />
-      {winners[0] && (
-        <PodiumStep height={300} left={166} width={198} bottom={180} winner={winners[0]} />
-      )}
-      {winners[1] && (
-        <PodiumStep height={260} left={0} width={166} bottom={115} winner={winners[1]} />
-      )}
-      {winners[2] && (
-        <PodiumStep height={260} left={363} width={171} bottom={85} winner={winners[2]} />
-      )}
+      <PodiumStep width={166} winner={winners[1]} ranking={2} />
+      <PodiumStep width={197} winner={winners[0]} ranking={1} />
+      <PodiumStep width={171} winner={winners[2]} ranking={3} />
     </Container>
   );
 };
