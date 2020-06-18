a904483816a60bad0b43700291851e46b8b5b2fe
Foucauld Degeorges <foucdeg@gmail.com>
2020-06-18 13:36:47 +0200

    auto restart game when player leaves or gets kicked at init phase

--- backend/core/service/room_service.py
+++ backend/core/service/room_service.py
@@ -4,8 +4,9 @@ from django.db import transaction
 from django.http import HttpResponse, HttpResponseBadRequest
 from django_eventstream import send_event
 
-from core.messages import NewAdminMessage, PlayerLeftMessage
-from core.models import Player, Room
+from core.messages import GameStartsMessage, NewAdminMessage, PlayerLeftMessage
+from core.models import GamePhase, Player, Room
+from core.service.game_service import initialize_game
 
 logger = logging.getLogger(__name__)
 
@@ -39,6 +40,8 @@ def remove_player_from_room(room_id: str, player_id: str):
             "message",
             PlayerLeftMessage(player, needs_new_admin).serialize(),
         )
+
+        # Handle possible new admin
         if needs_new_admin:
             new_admin = room.players.all().first()
 
@@ -55,5 +58,40 @@ def remove_player_from_room(room_id: str, player_id: str):
                     "message",
                     NewAdminMessage(new_admin).serialize(),
                 )
+
+        # If at init state, restart game while reusing preexisting initial sentences
+        if (
+            room.current_game is not None
+            and room.current_game.phase == GamePhase.INIT.value
+        ):
+            # Create new game with same params
+            new_game = initialize_game(
+                room,
+                [player.uuid.hex for player in room.players.all()],
+                room.current_game.round_duration,
+                room.current_game.draw_own_word,
+            )
+
+            # Apply any existing sentences on pads to new pad
+            existing_sentences = [
+                (pad.initial_player.uuid, pad.sentence)
+                for pad in room.current_game.pads.all()
+                if pad.sentence is not None
+            ]
+            for player_id, existing_sentence in existing_sentences:
+                for new_pad in new_game.pads.all():
+                    if new_pad.initial_player.uuid == player_id:
+                        new_pad.sentence = existing_sentence
+                        new_pad.save()
+
+            # Switch to new game
+            room.current_game = new_game
+            room.save()
+
+            send_event(
+                "room-%s" % room.uuid.hex,
+                "message",
+                GameStartsMessage(new_game).serialize(),
+            )
 
     return HttpResponse(status=200)
