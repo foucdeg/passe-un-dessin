e6ea8892a1650a7483b914317ecd6f272fda1c6b
Foucauld Degeorges <foucdeg@gmail.com>
2020-07-26 22:47:47 +0200

    add some SEO

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -30,14 +30,20 @@ module.exports = {
       }),
     );
 
+    const SitemapPlugin = require('sitemap-webpack-plugin').default;
+    const paths = ['/'];
+    config.plugins.push(
+      new SitemapPlugin('https://passe-un-dessin.fouc.net', paths, { lastmod: true }),
+    );
+
     return config;
   },
-  devServer: function(configFunction) {
+  devServer: function (configFunction) {
     // Return the replacement function for create-react-app to use to generate the Webpack
     // Development Server config. "configFunction" is the function that would normally have
     // been used to generate the Webpack Development server config - you can use it to create
     // a starting configuration to then modify instead of having to create a config from scratch.
-    return function(proxy, allowedHost) {
+    return function (proxy, allowedHost) {
       // Create the default config by calling configFunction with the proxy/allowedHost parameters
       const defaultConfig = configFunction(proxy, allowedHost);
       return {
