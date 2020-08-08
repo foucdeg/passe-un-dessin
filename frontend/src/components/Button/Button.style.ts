c51561f427eac8964d364d65fb0d1ffa2e28c071
Foucauld Degeorges <foucdeg@gmail.com>
2020-08-08 12:51:19 +0200

    Rework Button to accept being a Link

--- frontend/src/components/Button/Button.style.ts
+++ frontend/src/components/Button/Button.style.ts
@@ -1,7 +1,8 @@
 import styled, { css } from 'styled-components';
 import { colorPalette, fontSize, fontFamily } from 'stylesheet';
+import { Link } from 'react-router-dom';
 
-const Button = styled.button`
+const commonStyles = css`
   height: 51px;
   padding: 16px 32px;
   font-family: ${fontFamily.main};
@@ -9,25 +10,37 @@ const Button = styled.button`
   letter-spacing: 0.05em;
   background: linear-gradient(90deg, #ff9314 0%, #ff0080 100%);
   font-size: ${fontSize.medium};
-  cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
   box-shadow: 4px 2px 10px 2px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
   border: none;
   border-radius: 28px;
   color: ${colorPalette.white};
   outline: none;
-  ${(props) =>
-    props.disabled &&
-    css`
-      color: ${colorPalette.textGrey};
-      pointer-events: none;
-      background: ${colorPalette.backgroundGrey};
-      border: 2px solid ${colorPalette.textGrey};
-      box-shadow: none;
-    `}
+  display: flex;
+  align-items: center;
 
   &:active {
     box-shadow: inset -4px -2px 10px 2px rgba(0, 0, 0, 0.15);
   }
 `;
 
-export default Button;
+export const BaseStyledButton = styled.button`
+  ${commonStyles}
+  ${(props) =>
+    props.disabled &&
+    css`
+      cursor: default;
+      color: ${colorPalette.textGrey};
+      pointer-events: none;
+      background: ${colorPalette.backgroundGrey};
+      border: 2px solid ${colorPalette.textGrey};
+      box-shadow: none;
+    `}
+`;
+BaseStyledButton.displayName = 'BaseStyledButton';
+
+export const BaseStyledLink = styled(Link)`
+  ${commonStyles}
+  text-decoration: none;
+`;
+BaseStyledLink.displayName = 'BaseStyledLink';
