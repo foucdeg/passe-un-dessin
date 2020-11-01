5d16f214a96df7046de33a875a95c91c6e071977
Foucauld Degeorges <foucdeg@gmail.com>
2020-11-01 14:02:56 +0100

    wip: fix config

--- backend/passe_un_dessin/asgi.py
+++ backend/passe_un_dessin/asgi.py
@@ -6,8 +6,23 @@ defined in the ASGI_APPLICATION setting.
 import os
 
 import django
-from channels.routing import get_default_application
+import django_eventstream
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "passe_un_dessin.settings.prod")
 django.setup()
-application = get_default_application()
+# application = get_default_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": URLRouter(
+            [
+                url(r"^events/", URLRouter(django_eventstream.routing.urlpatterns),),
+                url(r"", get_asgi_application()),
+            ]
+        )
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
