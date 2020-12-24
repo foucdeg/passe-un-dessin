c6589a9485796471089688825c7c5d21d64abcee
RobotScribe <quentinso@theodo.fr>
2020-12-24 01:45:12 +0100

    :sparkles: Create twitch components

---
+++ frontend/src/pages/Home/components/TwitchModal/TwitchModal.style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import Header3 from 'atoms/Header3';
+import { colorPalette } from 'stylesheet';
+import Modal from 'components/Modal';
+
+export const Header = styled(Header3)`
+  color: ${colorPalette.purple};
+  margin-bottom: 20px;
+  align-self: flex-start;
+  font-weight: bold;
+`;
+Header.displayName = 'Header';
+
+export const Content = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+Content.displayName = 'Content';
+
+export const StyledModal = styled(Modal)`
+  width: 800px;
+  padding: 20px;
+`;
+StyledModal.displayName = 'StyledModal';
---
+++ frontend/src/pages/Home/components/TwitchStream/TwitchStream.style.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import BareAnchor from 'atoms/BareAnchor';
+import { colorPalette } from 'stylesheet';
+import { ReactComponent as EyeIcon } from 'assets/eye.svg';
+
+export const IMAGE_HEIGHT = 180; // Should be multiple of 9
+export const IMAGE_WIDTH = (IMAGE_HEIGHT * 16) / 9;
+
+export const StreamImage = styled.img`
+  width: ${IMAGE_WIDTH}px;
+  height: ${IMAGE_HEIGHT}px;
+`;
+
+export const StyledBareAnchor = styled(BareAnchor)`
+  width: ${IMAGE_WIDTH}px;
+  margin: 10px 30px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StreamerName = styled.div`
+  font-weight: bold;
+`;
+
+export const StreamTitle = styled.div`
+  font-weight: bold;
+`;
+
+export const StyledEye = styled(EyeIcon)`
+  .eye {
+    fill: ${colorPalette.orange};
+  }
+`;
+
+export const StreamViewerCount = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${colorPalette.orange};
+  font-weight: bold;
+
+  > :first-child {
+    margin-right: 5px;
+  }
+`;
