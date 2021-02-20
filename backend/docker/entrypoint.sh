4c5014267d49ad79919a1edc80b337d7b8ee17b5
Foucauld Degeorges <foucdeg@gmail.com>
2021-02-20 19:00:33 +0100

    use gunicorn cause dapne broke

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -3,4 +3,4 @@ python manage.py collectstatic --noinput
 python manage.py compilemessages
 python manage.py migrate
 
-daphne -b 0.0.0.0 -p 9001 passe_un_dessin.asgi:application
+gunicorn passe_un_dessin.wsgi:application -b 0.0.0.0:9001
