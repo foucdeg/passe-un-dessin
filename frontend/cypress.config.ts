232adc8a376605cfdca06f4f4eda0ade8a3cf7c8
Foucauld Degeorges <foucauld.degeorges@cenareo.com>
2022-06-24 15:49:07 +0200

    adapt Cypress config

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  defaultCommandTimeout: 10000,
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  env: {
+    BACKEND_CONTAINER_NAME: 'passe-un-dessin_backend',
+  },
+  e2e: {
+    baseUrl: 'https://localhost:3000',
+    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
+  },
+});
